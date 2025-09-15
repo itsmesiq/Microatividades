3cbc8de7f060b5c08c9ed1ba86eb2d74a1348fd1
Ana Siqueira <anasiqueira.serpentis@gmail.com>
2025-09-14 21:39:54 -0300

    First commit, microatividades

---
+++ estruturas_repeticao1.py
@@ -0,0 +1,8 @@
+entrada_idade = ""
+
+while entrada_idade != "0":
+    entrada_idade = input("Digite um número qualquer ou 0 para sair: ")
+    if entrada_idade != "0":
+        print("Número digitado: " + entrada_idade)
+
+print("Programa encerrado.")
